bfd8190b23eb8e9bd1fcd1984833797b0b34be5e
Minh Nguyen <mnguyen284@gmail.com>
2022-03-26 02:44:54 +0700

    crud-pages

--- src/modules/crud-pages/templates/dist/src/composables/crud-pages/useListPage/useTableView.ts
+++ src/modules/crud-pages/templates/dist/src/composables/crud-pages/useListPage/useTableView.ts
@@ -11,6 +11,8 @@ type QTableColumnOriginal = NonNullable<
 export interface QTableColumn<T = unknown> extends QTableColumnOriginal {
   field: string | ((row: T) => unknown);
   format?: (val: unknown, row: T) => unknown;
+  style?: string | ((row: T) => string);
+  classes?: string | ((row: T) => string);
 }
 
 export type QTablePagination = NonNullable<QTable['pagination']>;
