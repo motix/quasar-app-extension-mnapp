5667d7366a4be3b63d6ca58275249fbf9ecb47f9
Minh Nguyen <mnguyen284@gmail.com>
2022-10-27 23:32:15 +0700

    vendors - fontawesome

--- src/modules/vendors/uninstall.js
+++ src/modules/vendors/uninstall.js
@@ -3,6 +3,7 @@ const { defineUninstall } = require('..');
 module.exports = defineUninstall(function (api) {
   // Font Awesome Pro, vue-fontawesome
   api.removePath('src/boot/fontawesome-pro.ts');
+  api.removePath('src/types/vue-fontawesome.d.ts');
 
   api.onExitLog(
     '\x1b[32mvendors       • \x1b[0mPlease remove \x1b[47m\x1b[30m./.npmrc\x1b[0m if no longer used.'
