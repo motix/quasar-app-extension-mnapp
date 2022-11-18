dfc0fd54570082024751680cd8e9d7ddea8483b3
Minh Nguyen <mnguyen284@gmail.com>
2022-11-18 12:55:31 +0700

    map-paths

--- src/modules/map-paths/install.js
+++ src/modules/map-paths/install.js
@@ -8,6 +8,14 @@ module.exports.extendJsonFiles = {
   'tsconfig.json': {
     compilerOptions: {
       paths: {
+        'src/*': ['src/*'],
+        'app/*': ['*'],
+        'components/*': ['src/components/*'],
+        'layouts/*': ['src/layouts/*'],
+        'pages/*': ['src/pages/*'],
+        'assets/*': ['src/assets/*'],
+        'boot/*': ['src/boot/*'],
+        'stores/*': ['src/stores/*'],
         'utils/*': ['src/utils/*'],
         'models/*': ['src/models/*'],
         'api/*': ['src/api/*'],
