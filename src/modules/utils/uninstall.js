5dab8ce8ebfc5089b04952a9ee93328be3811b9a
Minh Nguyen <mnguyen284@gmail.com>
2023-04-05 16:31:33 +0700

    utils

--- src/modules/utils/uninstall.js
+++ src/modules/utils/uninstall.js
@@ -4,6 +4,8 @@ module.exports = defineUninstall(function (api) {
   api.removePath('src/types/automapper.d.ts');
   api.removePath('src/utils/automapper.ts');
   api.removePath('src/utils/calculation.ts');
+  api.removePath('src/utils/imageUrlToBase64.ts');
   api.removePath('src/utils/normalization.ts');
+  api.removePath('src/utils/stringContains.ts');
   api.removePath('src/utils/validation.ts');
 });
