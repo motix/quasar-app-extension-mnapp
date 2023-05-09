0e91fd81e3f0f7bbe04ceeed79f3f63490207794
Minh Nguyen <mnguyen284@gmail.com>
2023-05-10 03:28:36 +0700

    Minor

--- src/install.js
+++ src/install.js
@@ -1,37 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { InstallDefinition } = require('./lib/extension-wrappers');
 
-const fs = require('fs');
 const getPackageName = require('./lib/package-name');
+const { backupFile } = require('./lib/file-backup');
 const { reduceJsonFile } = require('./lib/json-helpers');
 const getModules = require('./modules');
 const { defineInstall } = getModules;
 
 module.exports = defineInstall(function (api) {
   /**
-   * @param {string} relativePath
-   */
-  function backupFile(relativePath) {
-    const filePath = api.resolve.app(relativePath);
-    const fileBackupPath = api.resolve.app(
-      `${relativePath.substring(
-        0,
-        relativePath.length - '.json'.length
-      )}-bk.json`
-    );
-
-    if (!fs.existsSync(fileBackupPath)) {
-      fs.copyFileSync(filePath, fileBackupPath);
-    }
-  }
-
-  /**
    * @type InstallDefinition[]
    */
   const modules = getModules('install');
 
-  backupFile('package.json');
-  backupFile('tsconfig.json');
+  backupFile(api, 'package.json');
+  backupFile(api, 'tsconfig.json');
 
   const packageName = getPackageName();
 
---
+++ src/lib/file-backup.d.ts
@@ -0,0 +1,8 @@
+import { SharedAPI } from '@quasar/app-vite';
+
+export declare function backupFile(api: SharedAPI, relativePath: string): void;
+export declare function backupAndDeleteFile(
+  api: SharedAPI,
+  relativePath: string
+): void;
+export declare function restoreFile(api: SharedAPI, relativePath: string): void;
