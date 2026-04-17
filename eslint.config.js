907578387edf55f233e22fd036235a8a8dec08f2
Quang Khánh <mnguyen284.ai@gmail.com>
2026-04-17 22:47:33 +0700

    `rootWorkspaceFormattingAndLinting()`

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import prettierSkipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default defineConfigWithVueTs(
+  {
+    /**
+     * ESLint requires "ignores" key to be the only one in this object
+     */
+    ignores: ['.yarn/', 'dev/', 'dist/', 'templates/', '.pnp.*'],
+  },
+
+  js.configs.recommended,
+
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports' }
+      ],
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        { allowShortCircuit: true, allowTernary: true },
+      ],
+    }
+  },
+  // https://github.com/vuejs/eslint-config-typescript
+  vueTsConfigs.recommendedTypeChecked,
+
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+
+      globals: {
+        ...globals.browser,
+        ...globals.node, // SSR, Electron, config files
+        process: 'readonly', // process.env.*
+      }
+    },
+
+    // add your custom rules here
+    rules: {
+      'prefer-promise-reject-errors': 'off',
+
+      // allow debugger during development only
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    }
+  },
+
+  prettierSkipFormatting
+)
